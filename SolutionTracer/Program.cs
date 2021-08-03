e3e83558032d0bd322271182a5b7b0b02a9569f6
Ramazan <ramazanaltinsoy@outlook.com>
2021-08-03 22:45:33 +0300

    Simple project added

---
+++ SolutionTracer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionTracer
+{
+    public class Program
+    {
+        private static readonly List<User> _users = new List<User>();
+
+        static void Main(string[] args)
+        {
+            var user1 = AddUser(1, "12345678912"); //valid phone number
+            var user2 = AddUser(2, "123"); //invalid phone number
+
+            var isPhoneValid = user1.IsPhoneValid();
+            isPhoneValid = user2.IsPhoneValid();
+        }
+
+        private static User AddUser(int id, string phone)
+        {
+            var user = new User(id, phone);
+            _users.Add(user);
+            return user;
+        }
+    }
+}
---
+++ SolutionTracer/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionTracer
+{
+    public class User
+    {
+        public int Id { get; }
+
+        public string Name => "User " + Id;
+
+        public string Phone { get; }
+
+        public User(int id, string phone)
+        {
+            Id = id;
+            Phone = phone;
+        }
+
+        public bool IsPhoneValid()
+        {
+            if (Phone.Length == 11)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
